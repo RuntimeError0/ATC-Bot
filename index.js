d4ecf3c464a229b811236177121fc53d8cb08a40
xHurricane <molham.work9@gmail.com>
2024-02-17 21:58:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ bot.command("contests", async (ctx) => {
     const msg = ctx.message;
     const txt = msg.text;
     const platform = txt.substring(txt.indexOf(" ") + 1);
+    platform = platform.toLowerCase();
     var usaco = "https://clist.by/api/v4/contest/?username=RuntimeError0&api_key=f11119d090d20aecdb2835c60d564587b92ac06a&resource_id=25&upcoming=true&format=json";
     var codechef = "https://clist.by/api/v4/contest/?username=RuntimeError0&api_key=f11119d090d20aecdb2835c60d564587b92ac06a&resource_id=2&upcoming=true&format=json";
     var atcoder = "https://clist.by/api/v4/contest/?username=RuntimeError0&api_key=f11119d090d20aecdb2835c60d564587b92ac06a&resource_id=93&upcoming=true&format=json";
@@ -133,7 +134,7 @@ bot.command("contests", async (ctx) => {
         });
     }
 
-    else if(txt == "/contests" || txt == "/contests@atc_contestsBot") {
+    else if(txt == "/contests" || txt == "/contests@atc_contestsbot") {
         const tosend = await contests.getAllContests(7);
         bot.api.sendMessage(chatID, tosend, {
             parse_mode: "HTML",
